9db338b7528da7555defa7ac0181773c69b786a0
Javier de Pedro López <droidpl@github.com>
2021-03-12 16:13:19 +0100

    Update ghec-audit-log-utils.js
    Co-authored-by: Stefan Stölzle <stoe@github.com>

--- ghec-audit-log-utils.js
+++ ghec-audit-log-utils.js
@@ -15,7 +15,7 @@ function validateInput (program, config) {
   }
 
   // Validate correctness
-  const tokenRegex = /^[A-Za-z0-9_]+$/i
+  const tokenRegex = /^[g(p|o|u|s|r)1_]{0,1}[A-Za-z0-9_]+$/
   const base64Regex = '(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)'
   const orgRegex = /^[a-z\d]+(?:-?[a-z\d]+)*$/i
   const constraints = {
